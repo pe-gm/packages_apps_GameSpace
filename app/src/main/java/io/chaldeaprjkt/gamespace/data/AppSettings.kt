98bda0c5e65f34d95444bc21f2249c1dade3373b
Pranav Vashi <neobuddy89@gmail.com>
2022-06-01 01:23:42 +0530

    GameSpace: Add lock gesture tile
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -44,7 +44,6 @@ class AppSettings @Inject constructor(private val context: Context) {
         get() = db.getBoolean(KEY_HEADS_UP_DISABLE, true)
         set(it) = db.edit().putBoolean(KEY_HEADS_UP_DISABLE, it).apply()
 
-
     var noAutoBrightness
         get() = db.getBoolean(KEY_AUTO_BRIGHTNESS_DISABLE, true)
         set(it) = db.edit().putBoolean(KEY_AUTO_BRIGHTNESS_DISABLE, it).apply()
@@ -65,6 +64,10 @@ class AppSettings @Inject constructor(private val context: Context) {
         get() = db.getBoolean(KEY_ADB_DISABLE, false)
         set(it) = db.edit().putBoolean(KEY_ADB_DISABLE, it).apply()
 
+    var lockGesture
+        get() = db.getBoolean(KEY_LOCK_GESTURE, false)
+        set(value) = db.edit().putBoolean(KEY_LOCK_GESTURE, value).apply()
+
     companion object {
         const val KEY_HEADS_UP_DISABLE = "gamespace_heads_up_disabled"
         const val KEY_AUTO_BRIGHTNESS_DISABLE = "gamespace_auto_brightness_disabled"
@@ -72,5 +75,6 @@ class AppSettings @Inject constructor(private val context: Context) {
         const val KEY_STAY_AWAKE = "gamespace_stay_awake"
         const val KEY_RINGER_MODE = "gamespace_ringer_mode"
         const val KEY_ADB_DISABLE = "gamespace_adb_disabled"
+        const val KEY_LOCK_GESTURE = "gamespace_lock_gesture"
     }
 }
