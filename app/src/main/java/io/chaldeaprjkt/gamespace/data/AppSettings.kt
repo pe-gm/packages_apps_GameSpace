51deb70d7bd70c39eed54fa59b869a0a6232fab4
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-09 17:28:56 +0800

    GameSpace: Let's take advantage of reTicker
    * It looks good in landscape mode, why not use it as notification overlay?
      This brings back in-game notifications settings.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [@neobuddy89: Clean up strings labels.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -52,6 +52,10 @@ class AppSettings @Inject constructor(private val context: Context) {
         get() = db.getBoolean(KEY_STAY_AWAKE, false)
         set(value) = db.edit().putBoolean(KEY_STAY_AWAKE, value).apply()
 
+    var notificationMode: Int
+        get() = db.getString(KEY_NOTIFICATION_MODE, "2").toInt()
+        set(value) = db.edit().putString(KEY_NOTIFICATION_MODE, value.toString()).apply()
+
     var ringerMode: Int
         get() = db.getString(KEY_RINGER_MODE, "3").toInt()
         set(value) = db.edit().putString(KEY_RINGER_MODE, value.toString()).apply()
@@ -68,6 +72,7 @@ class AppSettings @Inject constructor(private val context: Context) {
         const val KEY_AUTO_BRIGHTNESS_DISABLE = "gamespace_auto_brightness_disabled"
         const val KEY_3SCREENSHOT_DISABLE = "gamespace_tfgesture_disabled"
         const val KEY_STAY_AWAKE = "gamespace_stay_awake"
+        const val KEY_NOTIFICATION_MODE = "gamespace_notification_mode"
         const val KEY_RINGER_MODE = "gamespace_ringer_mode"
         const val KEY_ADB_DISABLE = "gamespace_adb_disabled"
         const val KEY_LOCK_GESTURE = "gamespace_lock_gesture"
